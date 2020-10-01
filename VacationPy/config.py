09e009748c11f8d57776db9c774760640041faee
LenSin3 <lenpk3@yahoo.co.uk>
2020-09-30 17:37:19 -0700

    Completed maps on new_vacation.

--- VacationPy/config.py
+++ VacationPy/config.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
 weather_api_key = "64c295209eb926e46778e23b59160355"
+api_key = "fdd1c46720102753ff7ba370d5b2b1f3"
+API_KEY = "0e0469ad2ed9a4b9a942ac5535e059f8"
 
 # Google API Key
-g_key = "AIzaSyCZTczK51JOsTkGwoH1pPg94uj3BoDwixc"
+gkey = "AIzaSyCZTczK51JOsTkGwoH1pPg94uj3BoDwixc"
